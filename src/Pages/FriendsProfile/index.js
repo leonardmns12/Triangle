3a620a148bcf07e2c095e8dfba75076aa23fd58e
Nico Fernando <nicofernando39@gmail.com>
2020-08-13 00:09:51 +0700

    fixing UI

--- src/Pages/FriendsProfile/index.js
+++ src/Pages/FriendsProfile/index.js
@@ -5,6 +5,7 @@ import Content from '../../Component/Molekuls/Timeline';
 import { useDispatch , useSelector } from 'react-redux';
 import LeftLogo from '../../../assets/chatWindow/left.svg';
 import ChatIcon from '../../../assets/navigation/comment.svg';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const FriendsProfile = ({navigation}) => {
     const gtchat = (screen) => {
@@ -25,18 +26,18 @@ const FriendsProfile = ({navigation}) => {
                 </TouchableOpacity>
                 <Text style={[style.headerText,{}]}>Profile</Text>
             </View>
-            <View style={{flex:0.6, backgroundColor:'#e8e6e6', position:'relative'}}>
+            <View style={{flex:0.5, backgroundColor:'#e8e6e6', position:'relative'}}>
               <View style={{position:'relative'}}>
-                <View style={{flexDirection:'row',marginTop:10, marginBottom:7}}>
+                <View style={{flexDirection:'row',marginTop:5, marginBottom:7}}>
                     <View style={[style.profileimg ,{}]}></View>
                     <Text style={[style.profilename, {}]}>Nico</Text>
                 </View>
                 <View style={{width:220, marginBottom: 50}}>
                     <Text style={[style.bio , {}]}>Better</Text>
                 </View>
-                <TouchableOpacity style={{flexDirection: 'row', width:100, height:30, backgroundColor:'#1BB0DF', marginLeft:140, borderRadius:50}}>
+                <TouchableOpacity style={{flexDirection: 'row', width:100, height:30, backgroundColor:'#1BB0DF', marginLeft:140, borderRadius:50, shadowOpacity:1}}>
                     <View style={{marginLeft:10,paddingTop:4}}>
-                    <ChatIcon  width = {20} height = {20} />
+                    <Icon name='message' color='#FFFFFF' size={20} />
                     </View>
                     <Text style={{marginLeft: 10, paddingTop:4, fontFamily:'ITCKRISTEN', color:'#FFFFFF'}}>
                         Chat
@@ -61,17 +62,17 @@ const FriendsProfile = ({navigation}) => {
 
 const style = StyleSheet.create({
     header: {
-        height : 57,
+        height : 55,
         backgroundColor : '#1BB0DF',
         flexDirection : 'row'
     },
-    // headerText: {
-    //     fontSize : 20,
-    //     fontFamily : 'ITCKRISTEN',
-    //     color : '#000000',
-    //     paddingTop : 15,
-    //     marginLeft: '10%',
-    // },
+    headerText: {
+        fontSize : 20,
+        fontFamily : 'ITCKRISTEN',
+        color : '#000000',
+        paddingTop : 15,
+        marginLeft: '10%',
+    },
     profileimg : {
         width: 50,
         backgroundColor: '#FFFFFF',
