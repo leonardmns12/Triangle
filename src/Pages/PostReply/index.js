1abf1c54753de3e9fb0cddf7a44deb1258d8319d
leonardmns12 <54368620+leonardmns12@users.noreply.github.com>
2020-08-19 21:11:12 +0700

    halleluya

--- src/Pages/PostReply/index.js
+++ src/Pages/PostReply/index.js
@@ -1,61 +1,16 @@
 import React from 'react';
-import {View , Text , StyleSheet , TouchableOpacity , ScrollView} from 'react-native'
-import PostComment from '../../Component/Molekuls/PostReplyComment/';
-import { TextInput } from 'react-native-gesture-handler';
-const PostReply = () => {
+import { View , Text } from 'react-native'
+const PostReplyComment = ({name , time , content}) => {
     return(
-        <View style={{backgroundColor:'white'}}>
-            <View style={[styles.header,{}]}>
-                <TouchableOpacity style={{paddingLeft:18, paddingTop:11}}> 
-                </TouchableOpacity>
-                <TouchableOpacity style={{position:'absolute' , right:'3%', top:'8%'}}>
-                </TouchableOpacity>
-                <Text style={[styles.headerText,{}]}>Timeline</Text>
-            </View>
-            <View>
-                <View style={{flexDirection:'row', paddingLeft:30}}>
-                    <View style={{height:30 , width:30 , borderRadius:30, borderWidth:1}}></View>
-                    <Text style={{marginLeft:30, justifyContent:'center' , alignItems:'center'}}>Leonard Monosa</Text>
-                </View>
-                <View>
-                    <Text style={{padding:30}}>Some friend being wise by adding more friend</Text>
-                </View>
-                <View style={{flexDirection:'row' , borderBottomWidth:1}}>
-                    <Text>19:00</Text>
-                    <Text style={{position:'absolute', right:0}}>4</Text>
-                </View>
-            </View>
-
-            <ScrollView style={{backgroundColor:'white'}}>
-                <PostComment name="Kent anderson" content="mantap" time="19.00"/>
-
-            </ScrollView>
-            <View style={{flexDirection:'row' , backgroundColor:'white',padding:30,}}>
-                <TextInput placeholder="Ketik Disini" style={{borderWidth:1, borderColor:'black', width:'60%', borderRadius:10}} ></TextInput>
-                <View style={{height:30 , width:30 , borderRadius:30, borderWidth:1, backgroundColor:'blue'}}></View>
+        <View style={{flexDirection:'row'}}>
+            <View style={{height:30 , width:30 , borderWidth:1, borderRadius: 30}}></View>
+            <View style={{marginLeft:10}}>
+                <Text style={{fontWeight:"bold", fontSize:16}}>{name}</Text>
+                <Text>{content}</Text>
+                <Text style={{fontSize:11}}>{time}</Text>
             </View>
         </View>
     )
 }
 
-const styles =  StyleSheet.create({
-    header : {
-        height : 57,
-        backgroundColor : '#1BB0DF',
-        flexDirection : 'row'
-    },
-    headerText : {
-        fontSize : 20,
-        fontFamily : 'ITCKRISTEN',
-        color : '#FFFFFF',
-        paddingTop : 15,
-        marginLeft: 20
-    },
-    timelinetext : {
-        fontSize : 18,
-        marginHorizontal : 18,
-        marginVertical : 15
-    }
-})
-
-export default PostReply
+export default PostReplyComment;
