cf26dea80436bccfbe2b9d96a0fc0eec5ecbde61
unknown <wenny28firstiary@gmail.com>
2020-08-21 00:31:02 +0700

    post Reply comment done

--- src/Pages/PostReply/index.js
+++ src/Pages/PostReply/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { View , Text } from 'react-native'
-const PostReplyComment = ({name , time , content}) => {
+import {View , Text , StyleSheet , TouchableOpacity , ScrollView } from 'react-native'
+import PostComment from '../../Component/Molekuls/PostReplyComment/';
+import { TextInput } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import OptionIcon from 'react-native-vector-icons/SimpleLineIcons'
+const PostReply = () => {
     return(
-        <View style={{backgroundColor:'white'}}>
+        <View style={{backgroundColor:'white', flex:1}}>
             <View style={[styles.header,{}]}>
                 <TouchableOpacity style={{paddingLeft:18, paddingTop:11}}> 
                 </TouchableOpacity>
@@ -10,32 +14,47 @@ const PostReplyComment = ({name , time , content}) => {
                 </TouchableOpacity>
                 <Text style={[styles.headerText,{}]}>Timeline</Text>
             </View>
-            <View style={{padding:10, backgroundColor:"white"}}>
+            <View style={{marginTop:"2%", backgroundColor:"white"}}>
                 <View style={{flexDirection:'row', paddingLeft:10}}>
                     <View style={[styles.profilePict]}></View>
                     <Text style={[styles.profileName]}>Leonard Monosa</Text>
+                    <View style={{position:'absolute', right:0, padding:"0%" , flexDirection:'row', marginLeft:"2%"}}>
+                        <TouchableOpacity>
+                            <OptionIcon name="options-vertical" size={20}/>
+                        </TouchableOpacity>
+                    </View>
                 </View>
                 <View>
-                    <Text style={{padding:"8%"}}>Some friend being wise by adding more friend</Text>
+                    <Text style={{marginTop:"3%", marginBottom: "0%", padding:"8%", backgroundColor:"#E9EBEE"}}>Some friend being wise by adding more friend</Text>
                 </View>
-                <View style={{flexDirection:'row' , borderBottomWidth:1, padding:10}}>
-                    <Text>19:00</Text>
-                    <Text style={{position:'absolute', right:0, padding:10}}>4</Text>
+                <View style={{flexDirection:'row' , borderBottomWidth:1, padding:10, borderBottomColor:"black"}}>
+                    <Text style={{fontFamily:'ITCKRISTEN', color:"grey"}}>19:00</Text>
+                    <View style={{position:'absolute', right:0, padding:10 , flexDirection:'row'}}>
+                    <Icon name="comment-text-multiple-outline" size={20} color="#1BB0DF" />
+                    <Text style={{marginLeft:5, color:"grey"}}>102</Text>
+                    </View>
+                </View>
+                <View style={{padding:"3%", flexDirection:"row", }}>
+                    <TouchableOpacity>
+                        <Text style={{}}>Load more comments</Text>
+                    </TouchableOpacity>
+                    <Text style={{marginLeft:"3%", padding:"1%", fontSize:10, color:"grey"}}>96 more</Text>
                 </View>
             </View>
-
+            
             <ScrollView style={{backgroundColor:'white', padding:10}}>
-                <PostComment name="Kent anderson" content="mantap" time="19.00"/>
-                <PostComment name="Kent anderson" content="mantap" time="19.00"/>
-                <PostComment name="Kent anderson" content="mantap" time="19.00"/>
-                <PostComment name="Kent anderson" content="mantap" time="19.00"/>
-                <PostComment name="Kent anderson" content="mantap" time="19.00"/>
-                <PostComment name="Kent anderson" content="mantap" time="19.00"/>
+                <PostComment name="Kent anderson" content="mantap" time="19.00"/>   
+                <PostComment name="Nico Fernando" content="gile" time="19.00"/>   
+                <PostComment name="Kent anderson" content="mantap" time="19.00"/>   
+                <PostComment name="Nico Fernando" content="gile" time="19.00"/>   
+                <PostComment name="Kent anderson" content="mantap" time="19.00"/> 
+                <PostComment name="Kent anderson" content="mantap" time="19.00"/> 
             </ScrollView>
-
-            <View style={{flexDirection:'row' , backgroundColor:'white', padding:10}}>
-                <TextInput placeholder="Ketik Disini" style={[styles.inputComment]}></TextInput>
-                <View style={{height:30 , width:30 , borderRadius:30, borderWidth:1, backgroundColor:'blue', margin:10}}></View>
+            <View style={{flexDirection:'row' , backgroundColor:'white', padding:10 , height:'auto'}}>
+                <TextInput placeholder="Type Here" style={[styles.inputComment]}/>
+                <TouchableOpacity>
+                    <Icon name="send-circle" size={38} color="#1BB0DF" style={{paddingTop:10}}/>
+                </TouchableOpacity>
             </View>
         </View>
     )
@@ -75,15 +94,16 @@ const styles =  StyleSheet.create({
     inputComment : {
         backgroundColor:"white", 
         width:'80%', 
-        borderRadius:10, 
-        padding:5, 
-        height:30, 
+        borderRadius:25, 
+        padding:10,  
+        height:35, 
         margin:10,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 1 },
         shadowOpacity: 0.8,
         shadowRadius: 2,  
-        elevation: 5
+        elevation: 5,
+
         
     }
 })
